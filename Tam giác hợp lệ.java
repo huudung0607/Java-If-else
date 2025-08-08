7780407caf73177e8e27c1f879e1d9ed94998b57
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-08 18:46:01 +0700

    Create Tam giác hợp lệ.java

---
+++ Tam giác hợp lệ.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class JavaApplication1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(); int b = sc.nextInt(); int c = sc.nextInt();
+        if ((a > 0 && b > 0 && c > 0) && (a + b > c && a + c > b && b + c > a)){
+            System.out.println("YES");
+        }
+        else System.out.println("NO");
+    }
+}
