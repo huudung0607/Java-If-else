b82a9e24b387f5c3ee6284954566696544fc95a4
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-12 17:58:11 +0700

    Create Làm tròn số.java

---
+++ Làm tròn số.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class JavaApplication1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+        int a = (int)n;
+        System.out.println(Math.round(n));
+    }
+}
