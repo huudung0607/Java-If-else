6865a497870ef4eb713338cc6b8a2e927395e241
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-12 18:08:49 +0700

    Create Tổ hợp chập 2 của N.java

---
+++ Tổ hợp chập 2 của N.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class JavaApplication1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(1L * n * (n - 1)/2);
+    }
+}
